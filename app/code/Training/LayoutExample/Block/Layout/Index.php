da3ce8fd06bf5d23dfa33d17cb5a2dd2ac7b263f
vaibhav.tiwari <vaibhav.tiwari@hbwsl.com>
2024-05-08 15:45:55 +0530

    video 8: setting title using Block

--- app/code/Training/LayoutExample/Block/Layout/Index.php
+++ app/code/Training/LayoutExample/Block/Layout/Index.php
@@ -6,5 +6,12 @@ use Magento\Framework\View\Element\Template;
 
 class Index extends Template
 {
-
+    protected function _prepareLayout(){
+        parent :: _prepareLayout();
+        
+        $pageMainTitle = $this->getLayout()->getBlock('page.main.title');
+        if ($pageMainTitle) {
+            $pageMainTitle->setPageTitle("Magento Developer");
+        }
+    }
 }
