cd7040fc56bf4c4893f9cab22c61c619f5668bc1
vaibhav.tiwari <vaibhav.tiwari@hbwsl.com>
2024-05-09 10:24:00 +0530

    video 13: removing a block from customer_account_login

--- app/etc/config.php
+++ app/etc/config.php
@@ -351,6 +351,7 @@ return [
         'Training_Example' => 1,
         'Training_LayoutExample' => 1,
         'Training_LayoutMoveExample' => 1,
+        'Training_LayoutRemoveExample' => 1,
         'Training_WidgetExample' => 1
     ]
 ];
