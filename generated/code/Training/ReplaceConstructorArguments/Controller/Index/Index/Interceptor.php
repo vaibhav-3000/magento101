f0e303dfe3f58c5c8c36e79fa2f67abd6da426ad
vaibhav.tiwari <vaibhav.tiwari@hbwsl.com>
2024-05-10 14:42:30 +0530

    video 24: replace constructor argument, print the original value in upper case http://vaibhav.magento.com/replace_constructor_arguments

--- generated/code/Training/ReplaceConstructorArguments/Controller/Index/Index/Interceptor.php
+++ generated/code/Training/ReplaceConstructorArguments/Controller/Index/Index/Interceptor.php
@@ -1,10 +1,10 @@
 <?php
-namespace Training\InjectablesAndNonInjectablesExample\Controller\Index\Index;
+namespace Training\ReplaceConstructorArguments\Controller\Index\Index;
 
 /**
- * Interceptor class for @see \Training\InjectablesAndNonInjectablesExample\Controller\Index\Index
+ * Interceptor class for @see \Training\ReplaceConstructorArguments\Controller\Index\Index
  */
-class Interceptor extends \Training\InjectablesAndNonInjectablesExample\Controller\Index\Index implements \Magento\Framework\Interception\InterceptorInterface
+class Interceptor extends \Training\ReplaceConstructorArguments\Controller\Index\Index implements \Magento\Framework\Interception\InterceptorInterface
 {
     use \Magento\Framework\Interception\Interceptor;
 
