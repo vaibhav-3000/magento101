aee46c1022adb253477e4ecb1cb53962bf97df48
vaibhav.tiwari <vaibhav.tiwari@hbwsl.com>
2024-05-10 12:11:56 +0530

    video 22: dependecy injection

--- app/etc/config.php
+++ app/etc/config.php
@@ -348,6 +348,7 @@ return [
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
+        'Training_DependencyExample' => 1,
         'Training_Example' => 1,
         'Training_HeadCssJs' => 1,
         'Training_HeaderFooterScripts' => 1,
