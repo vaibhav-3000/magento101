c45b989663ed9e0c8aa0e98126d920f4c7cc72b4
vaibhav.tiwari <vaibhav.tiwari@hbwsl.com>
2024-05-12 16:50:39 +0530

    video 40: mute the observer

--- pub/static/frontend/Magento/luma/en_GB/Magento_Security/js/escaper.js
+++
@@ -1,1 +0,0 @@
-/var/www/html/magento/vendor/magento/module-security/view/base/web/js/escaper.js
--- pub/static/frontend/Magento/luma/en_GB/Magento_Theme/js/theme.js
+++
@@ -1,1 +0,0 @@
-/var/www/html/magento/vendor/magento/theme-frontend-blank/Magento_Theme/web/js/theme.js
