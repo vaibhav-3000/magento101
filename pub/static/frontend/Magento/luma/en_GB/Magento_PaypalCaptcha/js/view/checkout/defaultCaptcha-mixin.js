d53bf35c8cc725e7f7f024a73a4c7759c10424c4
vaibhav.tiwari <vaibhav.tiwari@hbwsl.com>
2024-05-12 23:48:14 +0530

    video  48: schedule crons by config

--- pub/static/frontend/Magento/luma/en_GB/Magento_PaypalCaptcha/js/view/checkout/defaultCaptcha-mixin.js
+++
@@ -1,1 +0,0 @@
-/var/www/html/magento/vendor/magento/module-paypal-captcha/view/frontend/web/js/view/checkout/defaultCaptcha-mixin.js
--- pub/static/frontend/Magento/luma/en_GB/jquery/ui-modules/vendor/jquery-color/jquery.color.js
+++
@@ -1,1 +0,0 @@
-/var/www/html/magento/lib/web/jquery/ui-modules/vendor/jquery-color/jquery.color.js
